e22ec2f788dfc0071148b1537b3e79608acd696f
hdao35 <hdao35@gatech.edu>
2021-07-14 12:56:18 -0400

    add tutorial tooltip and refactor code organization

---
+++ Assets/Scripts/Misc/ButtonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    Player player;
+    Transform buttons;
+    Button summonButton, shopButton, adventureButton;
+    GameObject tooltip;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("GameController").GetComponent<Player>();
+        buttons = transform.Find("Buttons");
+        summonButton = buttons.Find("Summon Button").GetComponent<Button>();
+        shopButton = buttons.Find("Shop Button").GetComponent<Button>();
+        adventureButton = buttons.Find("Adventure Button").GetComponent<Button>();
+        tooltip = transform.Find("Tooltip").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool formationIsEmpty = true;
+        for (int i = 0; i < player.formation.Length; i++) {
+            if (player.formation[i] != -1) {
+                formationIsEmpty = false;
+                break;
+            }
+        }
+        if (formationIsEmpty) {
+            tooltip.SetActive(true);
+            adventureButton.interactable = false;
+            adventureButton.transform.GetChild(0).GetComponent<Text>().text = "Locked";
+        } else {
+            tooltip.SetActive(false);
+            adventureButton.interactable = true;
+            adventureButton.transform.GetChild(0).GetComponent<Text>().text = "Adventure";
+        }
+        if (Adventure.clearedAdventures >= 1) {
+            summonButton.interactable = true;
+            summonButton.transform.GetChild(0).GetComponent<Text>().text = "Summon";
+        } else {
+            summonButton.interactable = false;
+            summonButton.transform.GetChild(0).GetComponent<Text>().text = "Locked";
+        }
+        if (Adventure.clearedAdventures >= 2) {
+            shopButton.interactable = true;
+            shopButton.transform.GetChild(0).GetComponent<Text>().text = "Shop";
+        } else {
+            shopButton.interactable = false;
+            shopButton.transform.GetChild(0).GetComponent<Text>().text = "Locked";
+        }
+    }
+}
